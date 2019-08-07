8f33234d1ef57199b2089007c28a3094e212d100
Igor Borzenkov <iborzen@yandex.ru>
2019-08-07 14:47:15 +0800

    Dash was added

---
+++ CryptoApiSnippets/Samples/Blockchains/Transactions/GetInfoByTransactionHashDash.cs
@@ -0,0 +1,24 @@
+﻿using CryptoApisSdkLibrary.DataTypes;
+using System;
+
+namespace CryptoApiSnippets.Samples.Blockchains
+{
+  partial class BlockchainSnippets
+  {
+    public void GetInfoByTransactionHashDash()
+    {
+      var coin = BtcSimilarCoin.Dash;
+      var network = BtcSimilarNetwork.Testnet;
+      var transactionId = "9df2da1770e009bd7d75947fc9be07980bf30a67ae59a893cb89c1f4eeb57a19";
+
+      var manager = new CryptoManager(ApiKey);
+      var response = manager.Blockchains.Transaction.GetInfo(
+        coin, network, transactionId);
+
+      Console.WriteLine(string.IsNullOrEmpty(response.ErrorMessage)
+        ? "GetInfoByTransactionHashDash executed successfully, " +
+          $"block hash is {response.Transaction.BlockHash}"
+        : $"GetInfoByTransactionHashDash error: {response.ErrorMessage}");
+    }
+  }
+}
