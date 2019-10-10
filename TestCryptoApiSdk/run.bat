984681a32f438349fa01ba1f1839dd53733b4448
Igor Borzenkov <iborzenkov@gmail.com>
2019-10-10 20:55:05 +0800

    gitlab

--- TestCryptoApiSdk/run.bat
+++ TestCryptoApiSdk/run.bat
@@ -1,3 +1,4 @@
 rem dotnet test --filter TestCategory=Exchanges
 rem dotnet test --filter FullyQualifiedName~TestCryptoApiSdk.Exchanges.Test --results-directory TestResults --logger trx --verbosity detailed
 dotnet test --filter FullyQualifiedName~TestCryptoApiSdk.Exchanges.Test --results-directory ..\TestResults --logger trx --verbosity quiet
+dotnet test --filter FullyQualifiedName~TestCryptoApiSdk.Exchanges.Test2 --results-directory ..\TestResults --logger trx --verbosity quiet
