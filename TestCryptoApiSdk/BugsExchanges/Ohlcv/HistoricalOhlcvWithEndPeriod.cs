6ee95dcae0fc9bf41961038147cfc2586844f153
Igor Borzenkov <iborzenkov@gmail.com>
2019-10-12 16:56:34 +0800

    for testing

---
+++ TestCryptoApiSdk/BugsExchanges/Ohlcv/HistoricalOhlcvWithEndPeriod.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CryptoApisSdkLibrary.DataTypes;
+using CryptoApisSdkLibrary.DataTypes.Exceptions;
+using CryptoApisSdkLibrary.ResponseTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCryptoApiSdk.BugsExchanges.Ohlcv
+{
+    [TestClass]
+    public class HistoricalOhlcvWithEndPeriod : BaseCollectionTest
+    {
+        protected override ICollectionResponse GetAllList()
+        {
+            return Manager.Exchanges.Ohlcv.Historical(Symbol, Period, StartPeriod, EndPeriod);
+        }
+
+        protected override ICollectionResponse GetSkipList(int skip)
+        {
+            return Manager.Exchanges.Ohlcv.Historical(Symbol, Period, StartPeriod, EndPeriod, skip: skip);
+        }
+
+        protected override ICollectionResponse GetLimitList(int limit)
+        {
+            return Manager.Exchanges.Ohlcv.Historical(Symbol, Period, StartPeriod, EndPeriod, limit: limit);
+        }
+
+        protected override ICollectionResponse GetSkipAndLimitList(int skip, int limit)
+        {
+            return Manager.Exchanges.Ohlcv.Historical(Symbol, Period, StartPeriod, EndPeriod, skip, limit);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RequestException), "EndPeriod is less than StartPeriod.")]
+        public void TestIncorrectTimeInterval()
+        {
+            var startPeriod = new DateTime(DateTime.Now.Year, 01, 01);
+            var endPeriod = new DateTime(DateTime.Now.Year - 1, 01, 01);
+            Manager.Exchanges.Ohlcv.Historical(Symbol, Period, startPeriod, endPeriod);
+        }
+
+        [TestMethod]
+        public void TestIncorrectEndTimeFromFeature()
+        {
+            var endPeriod = new DateTime(DateTime.Now.Year + 1, 01, 01);
+            var response = Manager.Exchanges.Ohlcv.Historical(Symbol, Period, StartPeriod, endPeriod);
+
+            if (IsAdditionalPackagePlan)
+            {
+                AssertNullErrorMessage(response);
+                Assert.IsNotNull(response.Ohlcv, $"{nameof(response.Ohlcv)} must not be null");
+                AssertNotEmptyCollection(nameof(response.Ohlcv), response.Ohlcv);
+            }
+            else
+            {
+                AssertErrorMessage(response, "This endpoint has not been enabled for your package plan. Please contact us if you need this or upgrade your plan.");
+            }
+        }
+
+        private Symbol Symbol { get; } = new Symbol("5b7add17b2fc9a000157cc0a");
+        private Period Period { get; } = new Period("1day");
+        private DateTime StartPeriod { get; } = new DateTime(2019, 04, 15);
+        private DateTime EndPeriod { get; } = new DateTime(2019, 05, 01);
+    }
+}
---
+++ TestCryptoApiSdk/BugsExchanges/Rates/ExchangeRateInExchange.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using CryptoApisSdkLibrary.DataTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCryptoApiSdk.BugsExchanges.Rates
+{
+    [TestClass]
+    public class ExchangeRateInExchange : BaseTest
+    {
+        [TestMethod]
+        public void TestUsdBtc()
+        {
+            var response = Manager.Exchanges.Rates.GetOne(BaseAsset, QuoteAsset, Exchange);
+
+            AssertNullErrorMessage(response);
+            Assert.IsNotNull(response.Rate, $"{nameof(response.Rate)} must not be null");
+            Assert.AreEqual("5b1ea92e584bf50020130612", response.Rate.BaseAssetId);
+        }
+
+        [TestMethod]
+        public void TestUsdBtcTimeStamp()
+        {
+            var timeStamp = new DateTime(2019, 02, 03);
+            var response = Manager.Exchanges.Rates.GetOne(BaseAsset, QuoteAsset, Exchange, timeStamp);
+
+            AssertNullErrorMessage(response);
+            Assert.IsNotNull(response.Rate);
+        }
+
+        [TestMethod]
+        public void TestIncorrectTimeStampFromPast()
+        {
+            var timeStamp = new DateTime(1960, 01, 01);
+            var response = Manager.Exchanges.Rates.GetOne(BaseAsset, QuoteAsset, Exchange, timeStamp);
+
+            if (IsAdditionalPackagePlan)
+            {
+                AssertErrorMessage(response, "Your package plan includes only 365 days historical data. Please contact us if you need more or upgrade your plan.");
+                Assert.IsNull(response.Rate);
+            }
+            else
+            {
+                AssertErrorMessage(response, "This endpoint has not been enabled for your package plan. Please contact us if you need this or upgrade your plan.");
+            }
+        }
+
+        [TestMethod]
+        public void TestIncorrectTimeStampFromFeature()
+        {
+            var timeStamp = new DateTime(DateTime.Now.Year + 1, 01, 01);
+            var response = Manager.Exchanges.Rates.GetOne(BaseAsset, QuoteAsset, Exchange, timeStamp);
+
+            if (IsAdditionalPackagePlan)
+            {
+                AssertNullErrorMessage(response);
+                Assert.IsNull(response.Rate);
+            }
+            else
+            {
+                AssertErrorMessage(response, "This endpoint has not been enabled for your package plan. Please contact us if you need this or upgrade your plan.");
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "A BaseAsset of null was inappropriately allowed.")]
+        public void TestNullBaseAsset()
+        {
+            Manager.Exchanges.Rates.GetOne(baseAsset: null, quoteAsset: QuoteAsset);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "A QuoteAsset of null was inappropriately allowed.")]
+        public void TestNullQuoteAsset()
+        {
+            Manager.Exchanges.Rates.GetOne(BaseAsset, quoteAsset: null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "A BaseAsset.Id of null was inappropriately allowed.")]
+        public void TestNullBaseAssetId()
+        {
+            Manager.Exchanges.Rates.GetOne(new Asset(), quoteAsset: QuoteAsset);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "A QuoteAsset.Id of null was inappropriately allowed.")]
+        public void TestNullQuoteAssetId()
+        {
+            Manager.Exchanges.Rates.GetOne(BaseAsset, new Asset());
+        }
+
+        [TestMethod]
+        public void TestIncorrectBaseAsset()
+        {
+            var baseAsset = new Asset("QWE'EWQ");
+            var response = Manager.Exchanges.Rates.GetOne(baseAsset, QuoteAsset, Exchange);
+
+            AssertErrorMessage(response, "Exchange rate not found for the pair");
+            Assert.IsNull(response.Rate);
+        }
+
+        [TestMethod]
+        public void TestIncorrectQuoteAsset()
+        {
+            var quoteAsset = new Asset("QWE'EWQ");
+            var response = Manager.Exchanges.Rates.GetOne(BaseAsset, quoteAsset, Exchange);
+
+            AssertErrorMessage(response, "Exchange rate not found for the pair");
+            Assert.IsNull(response.Rate);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "An Exchange of null was inappropriately allowed.")]
+        public void TestNullExchange()
+        {
+            Manager.Exchanges.Rates.GetOne(BaseAsset, QuoteAsset, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "An Exchange.Id of null was inappropriately allowed.")]
+        public void TestNullExchangeId()
+        {
+            Manager.Exchanges.Rates.GetAny(BaseAsset, new Exchange());
+        }
+
+        [TestMethod]
+        public void TestIncorrectExchange()
+        {
+            var response = Manager.Exchanges.Rates.GetOne(BaseAsset, QuoteAsset, new Exchange("QWE'EWQ"));
+
+            AssertErrorMessage(response, "Exchange rate not found for the pair");
+            Assert.IsNull(response.Rate);
+        }
+
+        private Asset BaseAsset { get; } = new Asset("5b1ea92e584bf50020130612");
+        private Asset QuoteAsset { get; } = new Asset("5b1ea92e584bf50020130615");
+        private Exchange Exchange { get; } = new Exchange("5b4366dab98b280001540e16");
+    }
+}
