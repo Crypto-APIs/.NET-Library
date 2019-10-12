c3bca1d760558c52cf79f4bb0267dcf94fb416bf
Igor Borzenkov <iborzenkov@gmail.com>
2019-10-12 11:57:39 +0800

    gitlab

---
+++ TestCryptoApiSdk/Exchanges/Test2/AssetDetails.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CryptoApisSdkLibrary.DataTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCryptoApiSdk.Exchanges.Test2
+{
+    [TestClass]
+    public class AssetDetails : BaseTest
+    {
+        [TestMethod]
+        public void TestCorrect()
+        {
+            var asset = new Asset("5b755dacd5dd99000b3d92b2&");
+            var response = Manager.Exchanges.Info.AssetDetails(asset);
+
+            AssertNullErrorMessage(response);
+            Assert.IsNotNull(response.Asset, $"'{nameof(response.Asset)}' must not be null");
+            Assert.AreEqual("Ethereum", response.Asset.Name);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException), "An Asset of null was inappropriately allowed.")]
+        public void TestNull()
+        {
+            Manager.Exchanges.Info.AssetDetails(asset: null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception), "An Asset.Id of null was inappropriately allowed.")]
+        public void TestNullId()
+        {
+            Manager.Exchanges.Info.AssetDetails(new Asset());
+        }
+
+        [TestMethod]
+        public void TestIncorrectExchange()
+        {
+            var asset = new Asset("QWEE'WQ");
+            var response = Manager.Exchanges.Info.AssetDetails(asset);
+
+            AssertErrorMessage(response, "Entity not found");
+            Assert.IsNull(response.Asset, $"'{nameof(response.Asset)}' must be null");
+        }
+    }
+}
