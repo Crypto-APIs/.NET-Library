989392e36907580740a05d1d8e4ca7fc14f486eb
Igor Borzenkov <iborzenkov@gmail.com>
2019-10-19 20:36:57 +0800

    update

--- CryptoApisSnippets/Samples/Blockchains/WebhookNotifications/CreateTransactionPool.cs
+++ CryptoApisSnippets/Samples/Blockchains/WebhookNotifications/CreateTransactionPool.cs
@@ -12,7 +12,7 @@ namespace CryptoApisSnippets.Samples.Blockchains
 
       var manager = new CryptoManager(ApiKey);
       var response = manager.Blockchains.WebhookNotification.CreateTransactionPool<EthWebHookResponse>(
-          NetworkCoin.EthRopsten, url);
+          NetworkCoin.EthRopsten, url, "0x4ab47e7b0204d6b3bf0e956db14e63142b9b5ab8");
 
       Console.WriteLine(string.IsNullOrEmpty(response.ErrorMessage)
         ? "CreateTransactionPool executed successfully, " +
